5896a28c9108e6a536bf52784c00c192b8c7e27f
Baluji2497 <balug2497@gmail.com>
2022-03-29 01:12:06 +0530

    new project added

---
+++ org/Test/Test.java
@@ -0,0 +1,17 @@
+package org.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	
+	            public static void main(String[] args) {
+					
+	            	System.setProperty("webdriver.chrome.driver", "C:\\Users\\biass\\eclipse-workspace\\Project28Feb22\\driver\\chromedriver.exe");
+	            	
+	            	WebDriver.driver = new ChromeDriver();
+	            	
+	                                                                            	
+				}
+
+}
