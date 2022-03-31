8e4f590909dd9d786309c1e581d045f3ad005f00
Baluji2497 <balug2497@gmail.com>
2022-03-31 22:08:02 +0530

    new line added for signup

--- org/Test/Test.java
+++ org/Test/Test.java
@@ -11,6 +11,7 @@ public class Test {
 	            	
 	            	WebDriver.driver = new ChromeDriver();
 	            	
+	            	sysout.println();
 	                                                                            	
 				}
 
